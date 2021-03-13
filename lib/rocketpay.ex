d06e8916164a89d7cc73167b43028c1b56623bf1
Leao-E <eemanuelleao@gmail.com>
2021-03-13 00:32:19 -0300

    Create facade Rocketpay.deposit

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,5 +1,11 @@
 defmodule Rocketpay do
-  alias Rocketpay.Users.Create, as: UserCreate
 
+  # Creates an user and it's account with ballance 0.00
+  alias Rocketpay.Users.Create, as: UserCreate
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  # Performs a deposit to the given account
+  alias Rocketpay.Accounts.Deposit, as: Deposit
+  defdelegate deposit(params), to: Deposit, as: :call
+
 end
