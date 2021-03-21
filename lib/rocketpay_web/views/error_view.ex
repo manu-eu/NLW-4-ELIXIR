0a7f389392790c4b6577660d29e15fbe6cdf09f5
Leao-E <eemanuelleao@gmail.com>
2021-03-21 20:24:46 -0300

    Added to error_view.ex render function to handle result only with message instead changeset

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -16,9 +16,9 @@ defmodule RocketpayWeb.ErrorView do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
-  def render("400.json", %{result: %Changeset{} = changeset}) do
-    %{message: translate_errors(changeset)}
-  end
+  def render("400.json", %{result: %Changeset{} = changeset}), do: %{message: translate_errors(changeset)}
+
+  def render("400.json", %{result: message}), do: %{message: message}
 
   defp translate_errors(changeset) do
     Changeset.traverse_errors(changeset, fn {msg, opts} ->
