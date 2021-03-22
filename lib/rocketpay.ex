3da2d67fff900a93a522cda9cbcb49b82e101d9f
Leao-E <eemanuelleao@gmail.com>
2021-03-22 00:53:50 -0300

    Created facade for account withdraw opeartion

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,11 +1,14 @@
 defmodule Rocketpay do
 
-  # Creates an user and it's account with ballance 0.00
   alias Rocketpay.Users.Create, as: UserCreate
+  alias Rocketpay.Accounts.{Deposit, Withdraw}
+
+  # Creates an user and it's account with ballance 0.00
   defdelegate create_user(params), to: UserCreate, as: :call
 
   # Performs a deposit to the given account
-  alias Rocketpay.Accounts.Deposit, as: Deposit
   defdelegate deposit(params), to: Deposit, as: :call
 
+  # Performas a withdraw to the given account
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
