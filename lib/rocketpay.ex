d3f77df1eca02bfa3ac52379bff815f553afef8b
Leao-E <eemanuelleao@gmail.com>
2021-03-26 00:20:40 -0300

    Created account transaction facade

--- lib/rocketpay.ex
+++ lib/rocketpay.ex
@@ -1,7 +1,7 @@
 defmodule Rocketpay do
 
   alias Rocketpay.Users.Create, as: UserCreate
-  alias Rocketpay.Accounts.{Deposit, Withdraw}
+  alias Rocketpay.Accounts.{Deposit, Withdraw, Transaction}
 
   # Creates an user and it's account with ballance 0.00
   defdelegate create_user(params), to: UserCreate, as: :call
@@ -9,6 +9,9 @@ defmodule Rocketpay do
   # Performs a deposit to the given account
   defdelegate deposit(params), to: Deposit, as: :call
 
-  # Performas a withdraw to the given account
+  # Performs a withdraw to the given account
   defdelegate withdraw(params), to: Withdraw, as: :call
+
+  # Performs a transaction between two accounts
+  defdelegate transaction(params), to: Transaction, as: :call
 end
